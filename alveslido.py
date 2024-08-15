1de2abe513c26a8c81aa1c2b61d5d356c0d7b4b1
alvesesc <ezequiel.sa292@gmail.com>
2024-08-15 07:50:58 -0400

    s

---
+++ alveslido.py
@@ -0,0 +1,9 @@
+print("-OIIIE🤗")
+print("-Esse programa só funciona com a prof edini!!!")
+print("-Você é a prof edini🥺?????")
+sn=input("Digite 'S' para sim ou 'N' para nao")
+if sn == "S":
+    print("A edini é melhor que o ederson!!!!!!!!😎")
+else:
+    print("Saieh daqui entaoh😡🤬")
+    quit() 
