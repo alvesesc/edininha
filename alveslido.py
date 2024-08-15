97488c5ea107472e88400a9b424db04a4a951ae2
alvesesc <ezequiel.sa292@gmail.com>
2024-08-15 07:59:16 -0400

    time sleep adicionadohhh

--- alveslido.py
+++ alveslido.py
@@ -1,9 +1,14 @@
+import time
 print("-OIIIE🤗")
+time.sleep(0.5)
 print("-Esse programa só funciona com a prof edini!!!")
+time.sleep(0.5)
 print("-Você é a prof edini🥺?????")
 sn=input("Digite 'S' para sim ou 'N' para nao")
 if sn == "S":
     print("A edini é melhor que o ederson!!!!!!!!😎")
+    time.sleep(0.5)
+    print("Só verdades!!!!😘")
 else:
     print("Saieh daqui entaoh😡🤬")
     quit() 
